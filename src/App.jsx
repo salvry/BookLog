8b49bda68221898e435e142ab5751464de5b0b78
unknown <sallaryytty@gmail.com>
2024-01-17 18:37:21 +0200

    simplified the form and added a box that shows book details on hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
 
 const BookSelector = ({ selectBook, selectAudioBook }) => {
   return (
@@ -18,54 +19,44 @@ const BookForm = (props) => {
         <input onChange={props.handleAuthorChange} type="text" id="author" value={props.author} />
         <label htmlFor="title">Title</label>
         <input onChange={props.handleTitleChange} type="text" id="title" value={props.title} />
-        <label htmlFor="pages">Number of pages</label>
-        <input type="number" id="pages" onChange={props.handlePagesChange} value={props.pages} />
-        <button className="submit-button" type="submit">Add book</button>
-      </form>
-    </div>
-  );
-}
-
-const AudioBookForm = (props) => {
-  return (
-    <div className="bookform">
-      <form onSubmit={props.addBook}>
-        <label htmlFor="author">Author</label>
-        <input onChange={props.handleAuthorChange} type="text" id="author" value={props.author} />
-        <label htmlFor="title">Title</label>
-        <input onChange={props.handleTitleChange} type="text" id="title" value={props.title} />
+        {props.format === "audio" && 
+        <div className="audiobook-form">
         <label htmlFor="reader">Reader</label>
         <input type="text" id="reader" onChange={props.handleReaderChange} value={props.reader}/>
         <label>Duration</label>
         <input type="number" id="hours" onChange={props.handleHoursChange} value={props.hours} placeholder='Hours'/>
         <input type="number" id="minutes" onChange={props.handleMinutesChange} value={props.minutes} placeholder='Minutes'/>
         <button className="submit-button" type="submit">Add book</button>
-      </form>
+        </div>
+        }
+        {props.format==="print" && 
+        <div className="printbook-form">
+        <label htmlFor="pages">Number of pages</label>
+        <input type="number" id="pages" onChange={props.handlePagesChange} value={props.pages} />
+        <button className="submit-button" type="submit">Add book</button>
+        </div>
+      }
+    </form>
     </div>
   );
 }
 
-const BookList = (props) => {
+const BookListRow = ({book, showBook }) => {
   
   return (
-    <div>
-      <h2>{props.heading}</h2>
-      <ul>
-        {props.books.map(book => <li key={book.title}>
-         {book.author}: {book.title}
-        </li>
-        )}
-      </ul>
-      <p>{props.value}</p>
+    <div className="booklist-row">
+    <li><a onClick={showBook}>{book.author}: {book.title} 
+    <span className="book-details">
+      {book.format==="audio" ? ` ${Math.floor(book.length/60)} hours and ${book.length%60} minutes` : ` ${book.length} pages`}
+    </span>
+    </a></li>
     </div>
-  );
+  )
 }
-
 
 const App = () => {
   
-  const [book, setBook] = useState(false);
-  const [audioBook, setAudioBook] = useState(false);
+  const [bookFormat, setBookFormat] = useState('');
   const [books, setBooks] = useState([]);
   const [audioBooks, setAudioBooks] = useState([]);
   const [author, setauthor] = useState('');
@@ -77,13 +68,11 @@ const App = () => {
 
 
   const selectBook = () => {
-    setAudioBook(false);
-    setBook(true);
+    setBookFormat('print');
   } 
 
   const selectAudioBook = () => {
-    setBook(false);
-    setAudioBook(true);
+    setBookFormat('audio');
   }
     
   const handleAuthorChange = (event) => {
@@ -98,7 +87,6 @@ const App = () => {
     setPages(event.target.value);
   }
 
-
   const handleHoursChange = (event) => {
     setHours(event.target.value);
   }
@@ -111,26 +99,28 @@ const App = () => {
     setReader(event.target.value);
   }
 
-  
-  
   const addBook = (event) => {
     event.preventDefault();
-    const newBook = {author: author, title: bookTitle, length: Number(pages)};
-    book ? setBooks(books.concat(newBook)):setAudioBooks(audioBooks.concat(newBook));
-    setauthor('');
-    setBookTitle('');
-    setPages('');
+    if(bookFormat==="print"){
+      const newBook = {author: author, title: bookTitle, length: Number(pages), format:"print"};
+      setBooks(books.concat(newBook))
+      setauthor('');
+      setBookTitle('');
+      setPages('');
+      }
+    if(bookFormat==="audio"){
+      const newBook = {author: author, title: bookTitle, length: Number(hours)*60 + Number(minutes), reader: reader, format: "audio"};
+      setAudioBooks(audioBooks.concat(newBook));
+      setauthor('');
+      setBookTitle('');
+      setHours('');
+      setMinutes('');
+      setReader('');
+    }
   }
 
-  const addAudioBook = (event) => {
-    event.preventDefault();
-    const newBook = {author: author, title: bookTitle, length: Number(hours)*60 + Number(minutes), reader: reader};
-    book ? setBooks(books.concat(newBook)):setAudioBooks(audioBooks.concat(newBook));
-    setauthor('');
-    setBookTitle('');
-    setHours('');
-    setMinutes('');
-    
+  const showBook = (book) => {
+    console.log(book)
   }
 
   const pagesRead = books.reduce((acc, curr) => acc + curr.length, 0)
@@ -143,16 +133,22 @@ const App = () => {
       <div className="booklog">
         <div className="bookform">
           <BookSelector selectBook={selectBook} selectAudioBook={selectAudioBook}/>
-          {book && <BookForm author={author} title={bookTitle} pages={pages} addBook={addBook} handleAuthorChange={handleAuthorChange} 
+          {bookFormat === "print" && <BookForm format={bookFormat} author={author} title={bookTitle} pages={pages} addBook={addBook} handleAuthorChange={handleAuthorChange} 
           handleTitleChange={handleTitleChange} handlePagesChange={handlePagesChange} />}
-          {audioBook && <AudioBookForm author={author} title={bookTitle} addBook={addAudioBook} hours={hours} minutes={minutes}
-          handleAuthorChange={handleAuthorChange} handleTitleChange={handleTitleChange} 
+          {bookFormat === "audio" && <BookForm format={bookFormat} author={author} title={bookTitle} hours={hours} minutes={minutes} 
+          reader={reader} addBook={addBook} handleAuthorChange={handleAuthorChange} handleTitleChange={handleTitleChange} 
           handleHoursChange={handleHoursChange} handleMinutesChange={handleMinutesChange} handleReaderChange={handleReaderChange} />}
         </div>
       <div className="booklists">
-        {books.length > 0 && <BookList heading="Books" books={books} value={`You have read ${pagesRead} pages`}/>}
-        {audioBooks.length > 0 && <BookList heading="Audiobooks" books={audioBooks} 
-        value={`You have listened to audiobooks for ${hoursListened} hours and ${minutesListened} minutes`}/>}
+        {books.length > 0 && <><h2>Books</h2><ul>{books.map(book => <BookListRow key={book.title} book={book} 
+        showBook={()=>showBook(book)}/>)}</ul>
+        <p>You have read {pagesRead} pages</p>
+        </>}
+        {audioBooks.length > 0 && <><h2>Audiobooks</h2>
+        <ul>{audioBooks.map(book => <BookListRow key={book.title} book={book} showBook={()=>showBook(book)} length={book.length} />)}</ul>
+        <p>You have listened to audiobooks for {hoursListened} hours and {minutesListened} minutes</p>
+        </>
+        }
       </div>   
     </div>
   </div>
